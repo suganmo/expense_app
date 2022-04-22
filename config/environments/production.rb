e236776f2698c54b4a811ad0af8b4606ec8cd1b4
suganmo <suganmo84@gmail.com>
2022-04-23 02:55:42 +0900

    Update Gemfile for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,12 +25,11 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(harmony: true)
-  config.assets.css_compressor = :sass
+  # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
-  config.assets.digest = true
-  config.public_file_server.enabled = true # rails5〜
+  config.serve_static_assets = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
