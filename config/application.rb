cd4a59d0a9bab22bd42969b43cad32a7095f0e34
suganmo <suganmo84@gmail.com>
2022-04-23 01:04:27 +0900

    master

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,7 @@ module App2
     config.time_zone = 'Asia/Tokyo'
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
